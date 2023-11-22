c125f4dcd2e06eb30c3cc0c25cfecc3dc4603310
Vladimir Zhdanov <vladimir.z@mihoyo.com>
2023-11-22 10:57:27 +0800

    Minor exclusion list performance improvement

--- report_generator.py
+++ report_generator.py
@@ -50,6 +50,7 @@ def read_file_to_list(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         # Read lines into a list, stripping newline characters
         lines = [line.strip() for line in file]
+        lines.sort(key=len, reverse=True)   # To work better with exclusion list
 
     return lines
 
